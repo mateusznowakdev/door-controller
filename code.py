3b5974b93a548ee4890aecab0dc2d40386882ccc
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-11-17 01:58:58 +0100

    Change LCD backlight value

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 import board
 from digitalio import DigitalInOut
+from pwmio import PWMOut
 
 from adafruit_character_lcd.character_lcd import Character_LCD_Mono
 
@@ -9,6 +10,18 @@ lcd_d4 = DigitalInOut(board.GP7)
 lcd_d5 = DigitalInOut(board.GP6)
 lcd_d6 = DigitalInOut(board.GP5)
 lcd_d7 = DigitalInOut(board.GP4)
+lcd_bl = PWMOut(board.GP3)
 
 display = Character_LCD_Mono(rs=lcd_rs, en=lcd_en, db4=lcd_d4, db5=lcd_d5, db6=lcd_d6, db7=lcd_d7, columns=16, lines=2)
-display.message = "Test"
+
+
+def set_backlight(percentage: int) -> None:
+    lcd_bl.duty_cycle = percentage * 65535 // 100
+
+
+if __name__ == "__main__":
+    display.message = "Test"
+    set_backlight(25)
+
+    while True:
+        pass
