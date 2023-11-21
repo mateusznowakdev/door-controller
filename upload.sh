ba5df1c12b6e677a3080fa5d9c06f84a5f5d00ca
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-11-22 00:57:05 +0100

    Check CircuitPython version before upload

--- upload.sh
+++ upload.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-cp code.py /media/$USER/CIRCUITPY/code.py
+CPY_HOME=/media/$USER/CIRCUITPY
+CPY_VERSION="CircuitPython 8.2.8"
+
+if ! grep "$CPY_VERSION" "$CPY_HOME/boot_out.txt" >/dev/null
+then
+  echo "$CPY_VERSION must be installed"
+  exit 1
+fi
+
+cp code.py "$CPY_HOME/code.py"
