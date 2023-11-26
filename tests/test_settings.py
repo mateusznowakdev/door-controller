164d0ffaea5608bdf9a6ce018c6aa776f34f391d
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2023-11-26 16:14:38 +0100

    Create valid times list for sample schedule

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,9 +1,45 @@
-def run(sh: int, sm: int, eh: int, em: int, dur: int, div: int) -> None:
-    raise NotImplementedError()
+def minutes_to_time(m: int) -> tuple[int, int]:
+    return divmod(m, 60)
+
+
+def time_to_minutes(h: int, m: int) -> int:
+    return h * 60 + m
+
+
+def run(
+    start_hour: int,
+    start_minute: int,
+    end_hour: int,
+    end_minute: int,
+    duration: int,
+    divided_by: int,
+) -> tuple[tuple[int, int], ...]:
+    first_minute = time_to_minutes(start_hour, start_minute)
+    last_minute = time_to_minutes(end_hour, end_minute)
+
+    total_distance = last_minute - first_minute
+    distance = total_distance / (divided_by - 1)
+
+    entries = tuple(
+        minutes_to_time(int(first_minute + x * distance)) for x in range(divided_by)
+    )
+
+    return entries
 
 
 def test_day_schedule():
-    expected = ()
+    expected = (
+        (9, 0),
+        (9, 56),
+        (10, 53),
+        (11, 50),
+        (12, 46),
+        (13, 43),
+        (14, 40),
+        (15, 36),
+        (16, 33),
+        (17, 30),
+    )
     actual = run(9, 0, 17, 30, 456, 10)
 
     assert expected == actual
