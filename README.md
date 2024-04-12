f17ff44bfbc086a75c88f40e44360de8097ec4bd
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2024-04-12 19:33:31 +0200

    Add dependencies as submodules

--- README.md
+++ README.md
@@ -7,19 +7,16 @@ User manual can be found in [docs](./docs/README.md) directory.
 
 (instructions for Linux)
 
-1. Make sure recent version of Python and Git are installed.
-2. Clone the repository.
-3. Download and flash CircuitPython 9.0.3 using the official instructions.
+1. Clone the repository.
+    - `git clone --recurse-submodules https://github.com/mateusznowakdev/door-controller`
+2. Download and flash CircuitPython 9.0.3 using the official instructions.
     - [Download](https://adafruit-circuit-python.s3.amazonaws.com/bin/raspberry_pi_pico/en_US/adafruit-circuitpython-raspberry_pi_pico-en_US-9.0.3.uf2)
-4. Download mpy-cross binary to the `firmware` directory, rename it and make it
+3. Download mpy-cross binary to the `firmware` directory, rename it and make it
    executable.
     - [Download (amd64)](https://adafruit-circuit-python.s3.amazonaws.com/bin/mpy-cross/linux-amd64/mpy-cross-linux-amd64-9.0.3.static)
-5. Install circup:
-    - `pip install -U pip setuptools`
-    - `pip install circup`
-6. Build and upload the code to the CIRCUITPY drive
+4. Build and upload the code to the CIRCUITPY drive
     - `cd firmware`
-    - `./upload.sh --full`
+    - `./upload.sh`
 
 ## Language options
 
--- firmware/upload.sh
+++ firmware/upload.sh
@@ -5,6 +5,15 @@ set -e
 CPY_HOME=/media/$USER/CIRCUITPY
 CPY_VERSION="CircuitPython 9.0.3"
 
+LIB_FILES=(
+  lib/Adafruit_CircuitPython_24LC32/adafruit_24lc32.mpy
+  lib/Adafruit_CircuitPython_asyncio/asyncio
+  lib/Adafruit_CircuitPython_CharLCD/adafruit_character_lcd
+  lib/Adafruit_CircuitPython_DS3231/adafruit_ds3231.mpy
+  lib/Adafruit_CircuitPython_Register/adafruit_register
+  lib/Adafruit_CircuitPython_Ticks/adafruit_ticks.mpy
+)
+
 if [ ! -f mpy-cross ]
 then
   echo "mpy-cross binary must be present in the root directory. Exiting."
@@ -23,17 +32,13 @@ then
   exit 1
 fi
 
-find app -maxdepth 1 -name '*.py' -exec ./mpy-cross {} \;
+find app -name '*.py' -exec ./mpy-cross {} \;
+find lib -name '*.py' -exec ./mpy-cross {} \;
 
-mkdir -p "$CPY_HOME/app"
-
-if [[ $1 == "--full" ]]
-then
-  circup install -r requirements.txt
-fi
-
-rsync -cv app/*.mpy "$CPY_HOME/app"
-rsync -cv boot.py code.py safemode.py "$CPY_HOME"
+mkdir -p "$CPY_HOME/app" "$CPY_HOME/lib"
+rsync -crv --include="*/" --include="*.mpy" --exclude="*" app/*.mpy "$CPY_HOME/app/"
+rsync -crv --include="*/" --include="*.mpy" --exclude="*" "${LIB_FILES[@]}" "$CPY_HOME/lib/"
+rsync -crv boot.py code.py safemode.py "$CPY_HOME/"
 sync
 
 rm -r app/*.mpy
